5502e0397cc32bb9d1b7c083bb769532f13ec2af
Erik Ferrier <erik.ferrier@inmeta.no>
2016-08-18 15:12:23 +0200

    Masse ting

--- inc/1.php
+++ inc/1.php
@@ -1,1 +1,20 @@
-<div class="div">hei 1</div>
+<section id="questionaire">
+  <div class="row" id="question">
+    <div class="questionnumber col-sm-1">9/11</div>
+    <div class="questiontext col-sm-4">Er virksomheten organisert etter produktgrupper, eller etter 
+salgsfunksjoner mot kunder uavhengig av 
+produktgrupper?</div>
+  </div>
+  <div class="row col-sm-offset-1" id="answer">
+    <ul>
+      <li>Produktgrupper</li>
+      <li>Salgsfunksjoner</li>
+      <li>Annen organisering</li>
+      <li>Vet ikke</li>
+    </ul>
+  </div>
+  <div class="row" id="nav">
+   <div class="forrige col-sm-1 ">Forrige</div>
+   <div class="neste col-sm-1 col-sm-offset-4 ">Neste</div>
+  </div>
+</section>
