b4f71ae3089538859e9e96083ecb56c36f0d1c32
Erik Ferrier <erik.ferrier@inmeta.no>
2016-08-22 10:43:29 +0200

    footer - line-height answers

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,13 @@
 
 <footer>
-<div class="div">footer med logo og navn</div>
+	<div class="row">
+		<div class="logo" style="width:40px;">
+			<img src="img/logo_inmeta.png">
+		</div>
+	</div>
+	<div class="row">
+		Digital modenhet-barometer
+	</div>
 </footer>
 </body>
 </html>
